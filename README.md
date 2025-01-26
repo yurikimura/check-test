63c538e50187919a026cdfe5c2103560c1b9dad4
Yuri Kimura <yu@YurinoMacBook-Pro.local>
2025-01-26 17:36:55 +0900

    README作成

--- README.md
+++ README.md
@@ -4,13 +4,25 @@
 
 Docker ビルド
 
-1. `git clone リンク`
+1. `git clone https://github.com/yurikimura/check-test
+2. `docker-compose up -d --build`
 
-- Docker のビルドからマイグレーション、シーディングまでを記述する
+※ MySQL は、OS によって起動しない場合があるのでそれぞれの PC に合わせて docker-compose.yaml ファイルを編集してください。
+
+Laravel 環境構築
+
+1. `docker-compose exec php bash`
+2. `composer install`
+3. env.example ファイルから.env を作成し、環境変数を変更
+4. `php artisan key.generate`
+5. `php artisan migrate`
+6. `php artisan db:seed`
 
 ## 使用技術(実行環境)
 
-- Laravel 8.x(言語やフレームワーク、バージョンなどが記載されていると良い)
+- PHP 7.4.9
+- Laravel 8.83.8
+- MySQL 10.3.39
 
 ## ER 図
 
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -19,8 +19,8 @@
       </div>
       <div class="form__group-content">
         <div class="form__input--name">
-          <input type="text" name="first_name" placeholder="山田" value="{{ old('first_name') }}" required />
-          <input type="text" name="last_name" placeholder="太郎" value="{{ old('last_name') }}" required />
+          <input type="text" name="first_name" placeholder="例: 山田" value="{{ old('first_name') }}" required />
+          <input type="text" name="last_name" placeholder="例: 太郎" value="{{ old('last_name') }}" required />
         </div>
         <div class="form__error">
           @error('name')
@@ -92,7 +92,7 @@
       </div>
       <div class="form__group-content">
         <div class="form__input--text">
-          <input type="text" name="address" placeholder="東京都渋谷区世田谷1-2-3" value="{{ old('address') }}" required />
+          <input type="text" name="address" placeholder="例: 東京都渋谷区世田谷1-2-3" value="{{ old('address') }}" required />
         </div>
         <div class="form__error">
           @error('name')
@@ -144,6 +144,7 @@
             </div>
           </div>
         </div>
+
         <div class="form__button">
           <button class="form__button-submit" type="submit">確認画面</button>
         </div>
