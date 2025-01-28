c083cfdaa3828d1b93885a103c5b7e414ed8db73
Yuri Kimura <yu@YurinoMacBook-Pro.local>
2025-01-28 18:49:29 +0900

    admin画面の作成着手

--- src/resources/views/admin.blade.php
+++ src/resources/views/admin.blade.php
@@ -1,45 +1,58 @@
 @extends('layouts.appadmin')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+<link rel="stylesheet" href="{{ asset('css/admin.css') }}">
 @endsection
 
 @section('content')
 <div class="admin__content">
   <div class="admin-form__heading">
     <h2>Admin</h2>
-    <table>
+
+    <div class="filters">
+      <input type="text" class="search-input" placeholder="名前やメールアドレスを入力してください">
+
+      <select class="select-box">
+        <option value="">性別</option>
+        <option value="male">男性</option>
+        <option value="female">女性</option>
+      </select>
+
+      <select class="select-box">
+        <option value="">お問い合わせの種類</option>
+        <option value="product">商品について</option>
+        <option value="exchange">商品の交換について</option>
+      </select>
+
+      <select class="select-box">
+        <option value="">年/月/日</option>
+      </select>
+    </div>
+
+    <div class="actions">
+      <button class="btn btn-search">検索</button>
+      <button class="btn btn-reset">リセット</button>
+      <button class="btn btn-export">エクスポート</button>
+    </div>
+
+    <table class="data-table">
       <thead>
         <tr>
-          <th>ID</th>
-          <th>Last Name</th>
-          <th>First Name</th>
-          <th>Email</th>
-          <th>Gender</th>
-          <th>Tel</th>
-          <th>Address</th>
-          <th>Building</th>
-          <th>Category ID</th>
-          <th>Detail</th>
-          <th>Created At</th>
-          <th>Updated At</th>
+          <th>お名前</th>
+          <th>性別</th>
+          <th>メールアドレス</th>
+          <th>お問い合わせの種類</th>
+          <th>詳細</th>
         </tr>
       </thead>
       <tbody>
         @foreach($contacts as $contact)
         <tr>
-          <td>{{ $contact->id }}</td>
-          <td>{{ $contact->last_name }}</td>
-          <td>{{ $contact->first_name }}</td>
-          <td>{{ $contact->email }}</td>
+          <td>{{ $contact->last_name }} {{ $contact->first_name }}</td>
           <td>{{ $contact->gender }}</td>
-          <td>{{ $contact->tel }}</td>
-          <td>{{ $contact->address }}</td>
-          <td>{{ $contact->building }}</td>
+          <td>{{ $contact->email }}</td>
           <td>{{ $contact->category_id }}</td>
           <td>{{ $contact->detail }}</td>
-          <td>{{ $contact->created_at }}</td>
-          <td>{{ $contact->updated_at }}</td>
         </tr>
         @endforeach
       </tbody>
--- src/resources/views/thanks.blade.php
+++ src/resources/views/thanks.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Thanks</title>
+  <title>Check-Test</title>
   <link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
 </head>
 
