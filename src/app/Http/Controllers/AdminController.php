f3b6cfa7af01573023f8606dfbd67fc1a7d109ae
Yuri Kimura <yu@YurinoMacBook-Pro.local>
2025-02-06 10:03:58 +0900

    検索結果のpagenationを修正

--- src/app/Http/Controllers/AdminController.php
+++ src/app/Http/Controllers/AdminController.php
@@ -26,9 +26,21 @@ class AdminController extends Controller
 
     public function search(Request $request)
     {
+        $name = $request->input;
+        $gender = null;
+        switch ($request->gender) {
+            case "male":
+                $gender = "男性";
+                break;
+            case "female":
+                $gender = "女性";
+                break;
+            case "other":
+                $gender = "その他";
+                break;
+        };
+
         $category_id = $request->category_id;
-        $gender = $request->gender;
-        $name = $request->name;
 
         $query = Contact::query()
             ->where(function ($query) use ($name) {
